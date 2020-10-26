e2d67c99af3a9a353abfbf873632992369a3413d
Tristan <tristanshawnjohnson@gmail.com>
2020-10-25 21:30:25 -0700

    Implemented factorial and sum over array

--- src/recursion.js
+++ src/recursion.js
@@ -7,11 +7,36 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0){
+      return null;
+  }; 
+
+  if (n === 0){
+      return 1; 
+  } else {
+      return (n * factorial(n - 1));
+  }; 
+
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+    if (array.length === 0) {
+        return 0;
+    };
+
+    if (array.length === 1) {
+        return array[0];
+    };
+
+    if (array.length === 2) {
+        return array[0] + array[1];
+    } else {
+        for (var n = 0; n < array.length; n++) {
+        return array[n] + sum(array.slice(n + 1));
+        };
+    }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
